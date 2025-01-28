03845b69531f7a143473e1af15a959eed1650e65
Nicholas Ma <pointatnick@gmail.com>
2025-01-27 16:14:30 -0800

    Add Astro islands to the site

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://minhacks.dev',
-  base: '/web',
+  site: "https://minhacks.dev",
+  base: "/web",
+  integrations: [solidJs()],
 });
