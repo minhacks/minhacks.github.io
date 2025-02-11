d64983567b3b5dd3d52c89c7c2496c0c8fbf9021
Nicholas Ma <pointatnick@gmail.com>
2025-02-10 18:50:21 -0800

    Change base

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://minhacks.dev",
-  base: "/web",
+  // base: "/web",
   integrations: [solidJs()],
   vite: {
     plugins: [tailwindcss()],
